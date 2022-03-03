67ae45e771f8d96a503f4069cf8075e0e3244d85
rhea-mathew <rhea.mathew2@gmail.com>
2022-03-03 18:59:39 -0500

    Updated database.js with Journal, DailyTracker, Community, and Goals tables

--- Database.js
+++ Database.js
@@ -47,6 +47,21 @@ class Database {
         return await this.executeQuery(queryString);
     }
 
+    async editCompositeValues(table, identifier_array, identifier_value_array, columns, values) {
+        let updateString = '';
+        columns.forEach((item, i) => {
+            updateString += item + ' = "' + values[i] + '",';
+        });
+        // remove extra comma
+        updateString = updateString.slice(0, -1);
+
+        let queryString = 'UPDATE '  + '`' + table + '` SET ' + updateString
+            + ' WHERE ' + identifier_array[0] + ' = "' + identifier_value_array[0] + '" AND ' 
+            + '`' + identifier_array[1] + '` = "' + identifier_value_array[1] + '";';
+        console.log(queryString);
+        return await this.executeQuery(queryString);
+    }
+
     async readValues(table, identifier, identifier_value) {
         let queryString = 'SELECT * FROM ' + '`' + table + '`' + ' WHERE '
             + identifier + ' = "' + identifier_value + '";';
@@ -75,18 +90,77 @@ class Journal {
         let table = 'journal';
     }
 
-    // example add
-    async add(userid, category, message) {
-        return await db.insertValues(this.table, ['uid', 'category', 'message'], [userid, category, message])
+    async add(userID, category, message, timestamp) {
+        return await db.insertValues(this.table, ['uid', 'category', 'message', 'timestamp'], [userID, category, message, timestamp])
     }
 
-    async edit() {
-
+    async edit(userID, category, message, timestamp) { //checks the condition where column uid = userID
+        return await db.editCompositeValues(this.table, ['uid', 'timestamp'], [userID, timestamp], ['category', 'message'], [category, message])
     }
 
-    async delete() {
-
+    async delete(userID, timestamp) {
+        return await db.deleteCompositeValues(this.table, ['uid', 'timestamp'], [userID, timestamp])
     }
 }
 
+class DailyTracker {
+
+    constructor() {
+        let db = new Database();
+        let table = 'daily_tracker';
+    }
+
+    async add(userID, timestamp, stressLevel, anxietyLevel, energyLevel, mood, improvement) {
+        return await db.insertValues(this.table, ['uid', 'timestamp', 'stress_lvl', 'anxiety_lvl', 'energy_lvl', 'mood', 'improvement'], [userID, timestamp, stressLevel, anxietyLevel, energyLevel, mood, improvement])
+    }
+
+    async edit(userID, timestamp, stressLevel, anxietyLevel, energyLevel, mood, improvement) { //checks the condition where column uid = userID
+        return await db.editCompositeValues(this.table, ['uid', 'timestamp'], [userID, timestamp], ['stress_lvl', 'anxiety_lvl', 'energy_lvl', 'mood','improvement'], [stressLevel, anxietyLevel, energyLevel, mood, improvement], )
+    }
+
+    async delete(userID, timestamp) {
+        return await db.deleteCompositeValues(this.table, ['uid', 'timestamp'], [userID, timestamp])
+    }
+}
+
+class Community {
+
+    constructor() {
+        let db = new Database();
+        let table = 'community';
+    }
+
+    async add(groupID, hostUID, memberUIDarray) {
+        return await db.insertValues(this.table, ['id', 'host_uid', 'member_uid_array'], [groupID, hostUID, memberUIDarray])
+    }
+
+    async edit(groupID, hostUID, memberUIDarray) { //checks the condition where column id = groupID
+        return await db.editValues(this.table, 'id', groupID, ['host_uid', 'member_uid_array'], [hostUID, memberUIDarray])
+    }
+
+    async delete(groupID) { //deletes entire group
+        return await db.delete(this.table, 'id', groupID)
+    }
+}
+
+
+class Goals {
+
+    constructor() {
+        let db = new Database();
+        let table = 'goals';
+    }
+
+    async add(userID, timestamp, category, name, time, freq, notification, progress) {
+        return await db.insertValues(this.table, ['uid', 'timestamp', 'category', 'name', 'time', 'freq', 'notification', 'progress'], [userID, timestamp, category, name, time, freq, notification, progress])
+    }
+
+    async edit(userID, timestamp, category, name, time, freq, notification, progress) { //checks the condition where column uid = userID
+        return await db.editCompositeValues(this.table, ['uid', 'timestamp'], [userID, timestamp], ['timestamp', 'category', 'name', 'time', 'freq', 'notification', 'progress'], [timestamp, category, name, time, freq, notification, progress])
+    }
+
+    async delete(userID, timestamp) {
+        return await db.deleteCompositeValues(this.table, ['uid', 'timestamp'], [userID, timestamp])
+    }
+}
 export default Database;
