90d2da305f200724901ad181b30a900b9829a486
Ashray Chowdhry <ashraychowdhry@gmail.com>
2022-03-06 17:03:56 -0500

    sample query call

--- screens/tabs/Home.js
+++ screens/tabs/Home.js
@@ -2,13 +2,78 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Button from '../../components/Button';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CheckIn from '../Wellness/CheckIn';
 
+
+const Stack = createStackNavigator();
 
 export default function Home() {
+
+  const navigation = useNavigation();
+  const [marketOpen, setMarketOpen] = useState(null);
+
+ 
+
+  const HomeComponent = () => {
+
+    const navigateDailyQuiz = () => {
+      navigation.navigate('DailyCheckIn');
+      
+    };
+
+    const makeDBQuery = async () => {
+      var query = 'https://api.polygon.io/v1/marketstatus/now?apiKey=EepXZYpz1RiHneHcnRHQupa8To6g53Dv'
+      
+      var data;
+
+      try {
+          data = await axios.get(query)
+          .then(response => {
+
+              toast.show('The market is ' + response.data.market, {
+                  type: 'success',
+                  position: 'top',
+                  duration: 10000,
+                  offset: 30,
+                  animationType: 'slide-in'
+            });
+              
+              
+              if (response.data.market == 'closed') {
+                  setMarketOpen(false);
+              } else {
+                  setMarketOpen(true);
+              }
+          })
+      } catch (error) {
+          alert(error)
+      }
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}>You are now authenticated</Text>
+        <Button onPress={navigateDailyQuiz()}>Take Daily Quiz</Button>
+        <Button onPress={makeDBQuery()}>Take Daily Quiz</Button>
+      </SafeAreaView>
+    )
+    
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>You are now authenticated</Text>
-    </SafeAreaView>
+      <Stack.Navigator 
+        screenOptions={{
+            headerStyle: {
+              backgroundColor: '#BDE3DF'
+            },
+            headerTintColor: '#fff'
+        }} initialRouteName="Home">
+
+          <Stack.Screen name="Home" options={{headerShown: false}} component={HomeComponent} />
+          <Stack.Screen name="DailyCheckIn" component={CheckIn} />
+      </Stack.Navigator>
+      
   )
 }
 
