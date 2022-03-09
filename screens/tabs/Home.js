b560ff706c4eafd147de2e471510260db641f3f8
Bryce Acree <bacree3@gatech.edu>
2022-03-09 15:22:57 -0500

    added successful journal entry, working on resolving user.sub to resolve for query

--- screens/tabs/Home.js
+++ screens/tabs/Home.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Button from '../../components/Button';
 import axios from 'axios';
-import Database from '../../Database';
+import { Database } from '../../Database';
 import User from '../../User';
 
 export default function Home() {
