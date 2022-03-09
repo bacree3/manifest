0a0c37d4d976ce4562cc8144f7bc68055b9895b9
Bryce Acree <bacree3@gatech.edu>
2022-03-09 13:57:58 -0500

    added amplify dependency for successful get of current user attributes

--- User.js
+++ User.js
@@ -1,6 +1,7 @@
 // amplify api doc: https://aws-amplify.github.io/amplify-js/api/classes/authclass.html
 
 import Auth from '@aws-amplify/auth';
+import Amplify from '@aws-amplify/core';
 import awsconfig from './aws-exports';
 
 class User {
