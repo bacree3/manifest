135a9ba41dd4c864b877763d4a830fd74322d914
Bryce Acree <bacree3@gatech.edu>
2022-04-27 18:21:08 -0400

    removed unused import on settings

--- screens/tabs/Settings.js
+++ screens/tabs/Settings.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, TextInput, StyleSheet, Pressable, Switch, Platform, Keyboard, DevSetting, ScrollView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Pressable, Switch, Platform, DevSetting, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
